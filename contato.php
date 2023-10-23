913b0a0a79b699c5178389c126b6d8e933f1379e
Mateus Missfeld de Oliveira <missfeldmateus@gmail.com>
2023-10-23 18:45:20 -0300

    first-commit-blog
    Primeiro commit desse blog que criei com auxilio do curso, algumas mudanças vão ser feitas, mas inicialmente ficaria assim.

---
+++ contato.php
@@ -0,0 +1,8 @@
+<?php
+  include_once("templates/header.php");
+?>
+  <h1>Página de contato</h1>
+  <h3>Whatsapp <a class="tel" target="_blank" href="https://wa.me/+541125957146">+54 11 2595-7146</a> </h3>
+<?php
+  include_once("templates/footer.php")
+?>
